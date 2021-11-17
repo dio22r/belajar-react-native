570aa2618bdf82a13aa108ac20155d1a7c14e721
William <william.mulianto@gmail.com>
2021-11-17 09:11:01 +0700

    Move thunk to regular reducer

--- src/features/reducer/todos.tsx
+++ src/features/reducer/todos.tsx
@@ -1,4 +1,4 @@
-import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export interface todo {
@@ -23,15 +23,26 @@ export const fetchTodos = createAsyncThunk('todos/fetchTodos', async () => {
   return axios.get<todo[]>(baseUrl).then(response => response.data);
 });
 
-export const updateTodos = createAsyncThunk(
-  'todos/updateTodos',
-  async (todoId: number) => todoId,
-);
+// export const updateTodos = createAsyncThunk(
+//   'todos/updateTodos',
+//   async (todoId: number) => todoId,
+// );
 
 export const todosSlice = createSlice({
   name: 'todos',
   initialState,
-  reducers: {},
+  reducers: {
+    
+    updateTodos: (state, action: PayloadAction<number>) => {
+      
+      let todos = state.todos.find(todo => todo.id === action.payload);
+      if (todos) {
+        console.log("check dari reducers")
+        todos.completed = !todos.completed;
+      }
+      state.isLoading = false;
+    },
+  },
   extraReducers: builder => {
     builder
       .addCase(fetchTodos.pending, state => {
@@ -41,20 +52,15 @@ export const todosSlice = createSlice({
         state.isLoading = false;
         state.todos = action.payload;
       })
-      .addCase(updateTodos.pending, state => {
-        state.isLoading = true;
-      })
-      .addCase(updateTodos.fulfilled, (state, action) => {
-        let todos = state.todos.find(todo => todo.id === action.payload);
-        if (todos) {
-          todos.completed = !todos.completed;
-        }
-        state.isLoading = false;
-      });
+      // .addCase(updateTodos.pending, state => {
+      //   state.isLoading = true;
+      // })
+      // .addCase(updateTodos.fulfilled, (state, action) => {
+      // });
   },
 });
 
 // Action creators are generated for each case reducer function
-// export const {setChecked} = todosSlice.actions;
+export const {updateTodos} = todosSlice.actions;
 
 export default todosSlice.reducer;
